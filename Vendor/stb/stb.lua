edaa51c06cb30eeef200a7ae0f67cb593be52136
Erwann Messoah <tiwannbeats@gmail.com>
2023-05-18 09:20:39 +0200

    Moved stb to Hydro directory

---
+++ Vendor/stb/stb.lua
@@ -0,0 +1,38 @@
+project "stb"
+	kind "StaticLib"
+	staticruntime "On"
+	language "C"
+
+	targetdir(binaries)
+	objdir(intermediate)
+
+	files
+	{
+	    "stb.lua",
+		"src/stb.c",
+		"include/stb/stb_image.h",
+		"include/stb/stb_image_resize.h",
+		"include/stb/stb_image_write.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+	
+	
+    filter "System:Windows"
+        defines "_CRT_SECURE_NO_WARNINGS"
+
+	filter "Configurations:Debug"
+		runtime "Debug"
+		optimize "Off"
+		symbols "On"
+
+	filter "Configurations:Release"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+
+
+
