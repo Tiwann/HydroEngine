6a5eb2611d1bac5ecc7cbd5b4838eb94cf4eca82
Erwann Messoah <tiwannbeats@gmail.com>
2023-05-18 09:21:00 +0200

    Moved glad to Hydro directory

---
+++ Vendor/glad/glad.lua
@@ -0,0 +1,28 @@
+project "glad"
+	kind "StaticLib"
+	language "C"
+	staticruntime "On"
+	flags "MultiProcessorCompile"
+
+	targetdir(binaries)
+	objdir(intermediate)
+
+	files {
+		"include/glad/gl.h",
+		"include/KHR/khrplatform.h",
+		"src/gl.c",
+		"glad.lua"
+	}
+
+	includedirs "include"
+
+	filter "Configurations:Debug"
+		runtime "Debug"
+		optimize "Off"
+		symbols "On"
+
+	filter "Configurations:Release"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+
