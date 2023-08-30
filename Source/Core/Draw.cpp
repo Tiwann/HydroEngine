c61d874c8b60909943422415f96f516a9e115129
Erwann Messoah <tiwannbeats@gmail.com>
2023-08-30 13:28:36 +0200

    Immediate mode drawing classes for debugging

---
+++ Source/Core/Draw.cpp
@@ -0,0 +1,16 @@
+﻿#include "HydroPCH.h"
+#include "Draw.h"
+
+#include "Application.h"
+#include "Renderer.h"
+
+#include "shaderc/shaderc.hpp"
+
+void Hydro::Draw::Point(const glm::vec2& Position, float Radius, const Color& Color)
+{
+    Renderer& Renderer = Application::GetCurrentApplication().GetRenderer();
+}
+
+void Hydro::Draw::Point(float X, float Y, float Radius, const Color& Color)
+{
+}
---
+++ Source/Core/Draw.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "glm/vec2.hpp"
+#include "Macros.h"
+
+namespace Hydro
+{
+    class Color;
+    
+    class HYDRO_API Draw
+    {
+    public:
+        static void Point(const glm::vec2& Position, float Radius, const Color& Color);
+        static void Point(float X, float Y, float Radius, const Color& Color);
+    };
+}
