2b50637d7394b0591a3b1f6bd9c5515da0e72b05
Erwann Messoah <tiwannbeats@gmail.com>
2024-04-02 14:27:41 +0200

    Updated spdlog to latest version

---
+++ Vendor/spdlog/spdlog.lua
@@ -0,0 +1,31 @@
+group "HydroFramework"
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+	staticruntime "On"
+	flags "MultiProcessorCompile"
+	
+	targetdir(binaries)
+	objdir(intermediate)
+
+	files {
+		"include/**.h",
+		"src/**.cpp",
+		"spdlog.lua"
+	}
+	
+	defines { "SPDLOG_COMPILED_LIB" }
+
+	includedirs "include"
+
+	filter "Configurations:Debug*"
+		runtime "Debug"
+		optimize "Off"
+		symbols "On"
+
+	filter "Configurations:Release*"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+
