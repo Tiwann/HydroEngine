6ff7e9a305639ac956531184fb0348cf405ade31
Erwann Messoah <tiwannbeats@gmail.com>
2024-04-02 14:26:06 +0200

    Imported yaml-cpp

---
+++ Vendor/yaml-cpp/yaml-cpp.lua
@@ -0,0 +1,31 @@
+group "HydroFramework"
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+	flags "MultiProcessorCompile"
+
+	targetdir(binaries)
+	objdir(intermediate)
+
+	files {
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h",
+		"yaml-cpp.lua"
+	}
+
+	includedirs "include"
+
+	defines  "YAML_CPP_STATIC_DEFINE"
+
+	filter "Configurations:Debug*"
+		runtime "Debug"
+		optimize "Off"
+		symbols "On"
+
+	filter "Configurations:Release*"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
