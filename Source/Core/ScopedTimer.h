7bc7950c4d0abad6202377c0a3b0243e0edbed1c
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-11 02:53:07 +0200

    Create ScopedTimer.h

---
+++ Source/Core/ScopedTimer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "MulticastDelegate.h"
+
+namespace Hydro
+{
+    template<typename T = std::chrono::seconds>
+    class ScopedTimer
+    {
+    public:
+        using ScopedTimerFinishedDelegate = MulticastDelegate<void(float Duration)>;
+        ScopedTimerFinishedDelegate FinishedEvent;
+        
+        ScopedTimer(const ScopedTimerFinishedDelegate::DelegateType& OnFinished)
+        {
+            FinishedEvent.Bind(OnFinished);
+            m_Start = std::chrono::high_resolution_clock::now();
+        }
+        
+        ~ScopedTimer()
+        {
+            m_End = std::chrono::high_resolution_clock::now();
+            const auto Duration = duration_cast<T>(m_End - m_Start).count();
+            FinishedEvent.BroadcastChecked((float)Duration / T::period::den);
+            FinishedEvent.ClearAll();
+        }
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_End;
+    };
+}
