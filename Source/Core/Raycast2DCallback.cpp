a0cb1de8405757acbef79aee3d31a2595bbc22ea
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-20 16:17:22 +0200

    Update Raycast2DCallback.cpp

--- Source/Core/Raycast2DCallback.cpp
+++ Source/Core/Raycast2DCallback.cpp
@@ -1,6 +1,8 @@
 #include "HydroPCH.h"
 #include "Raycast2DCallback.h"
 
+#include <box2d/b2_fixture.h>
+
 namespace Hydro
 {
     float Raycast2DCallback::ReportFixture(b2Fixture* fixture, const b2Vec2& point, const b2Vec2& normal,
