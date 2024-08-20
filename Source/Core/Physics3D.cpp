e4ce7fea7ffa77b243b87ed9c7037bb15605f308
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-20 16:11:20 +0200

    Created Physics3D

---
+++ Source/Core/Physics3D.cpp
@@ -0,0 +1,8 @@
+﻿#include "HydroPCH.h"
+#include "Physics3D.h"
+
+namespace Hydro
+{
+    Vector3 Physics3D::Gravity = { 0.0f, -9.81f, 0.0f };
+    float Physics3D::TimeStep = 1.0f / 60.0f;
+}
---
+++ Source/Core/Physics3D.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Math/Vector3.h"
+
+namespace Hydro
+{
+    class Physics3D
+    {
+    public:
+        static Vector3 Gravity;
+        static float TimeStep;
+
+        
+    };
+}
