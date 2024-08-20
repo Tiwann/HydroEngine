3bbdaa8cf5e59483b3df14f7e578a95c0484b6d0
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-20 16:13:04 +0200

    Create SpriteSheet.h

---
+++ Source/Core/SpriteSheet.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace Hydro
+{
+    struct SpriteSheet
+    {
+        uint32_t NumRows;
+        uint32_t NumColumns;
+        uint32_t NumSprites;
+        uint32_t SpriteSize;
+    };
+}
