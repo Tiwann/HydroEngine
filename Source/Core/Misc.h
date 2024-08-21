a618f9b68862715df508ec071a9797f387b863c4
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-21 18:31:44 +0200

    Create Misc.h

---
+++ Source/Core/Misc.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "TypeTraits.h"
+
+namespace Hydro
+{
+    template<typename T, typename = EnableIfType<IsFloatingPointValue<T>>>
+    constexpr T FPS(int Val) { return 1.0f / (T)Val; }
+}
