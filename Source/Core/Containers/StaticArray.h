e83abe161523fa7d8fec09052e03c6e6e42cfe10
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-23 15:38:40 +0200

    Update StaticArray.h

--- Source/Core/Containers/StaticArray.h
+++ Source/Core/Containers/StaticArray.h
@@ -25,18 +25,18 @@ namespace Hydro
             }
         }
 
-        StaticArray(const std::initializer_list<T>& List) : m_Count(List.size())
+        StaticArray(const std::initializer_list<T>& List)
         {
             std::copy(List.begin(), List.end(), m_Data);
         }
         
-        StaticArray(ConstPointerType Data, SizeType Count) : m_Count(Count)
+        StaticArray(ConstPointerType Data, SizeType Count)
         {
             HYDRO_ASSERT(Count <= N, "Cannot fill in an Array<N> with more elements than N");
             std::copy(Data, Data + Count, m_Data);
         }
 
-        StaticArray(const StaticArray& Other) : m_Count(Other.m_Count)
+        StaticArray(const StaticArray& Other)
         {
             std::copy(Other.begin(), Other.end(), m_Data);
         }
@@ -49,7 +49,6 @@ namespace Hydro
                 return *this;
             
             std::copy(Other.begin(), Other.end(), m_Data);
-            m_Count = Other.m_Count;
             return *this;
         }
 
@@ -57,58 +56,38 @@ namespace Hydro
 
         ReferenceType operator[](SizeType Index)
         {
-            HYDRO_ASSERT(Index <= m_Count, "Index out of bounds");
+            HYDRO_ASSERT(Index <= N, "Index out of bounds");
             return m_Data[Index];
         }
 
         ConstReferenceType operator[](SizeType Index) const
         {
-            HYDRO_ASSERT(Index <= m_Count, "Index out of bounds");
+            HYDRO_ASSERT(Index <= N, "Index out of bounds");
             return m_Data[Index];
         }
 
         void SetAt(SizeType Index, ConstReferenceType Element)
         {
-            HYDRO_ASSERT(Index <= m_Count, "Index out of bounds");
+            HYDRO_ASSERT(Index <= N, "Index out of bounds");
             m_Data[Index] = Element;
         }
 
         ReferenceType GetAt(SizeType Index)
         {
-            HYDRO_ASSERT(Index <= m_Count, "Index out of bounds");
+            HYDRO_ASSERT(Index <= N, "Index out of bounds");
             return m_Data[Index];
         }
 
         ConstReferenceType GetAt(SizeType Index) const
         {
-            HYDRO_ASSERT(Index <= m_Count, "Index out of bounds");
+            HYDRO_ASSERT(Index <= N, "Index out of bounds");
             return m_Data[Index];
         }
 
-        void Add(ConstReferenceType Element)
-        {
-            HYDRO_ASSERT(m_Count < N, "Array is full");
-            m_Data[m_Count] = Element;
-            m_Count++;
-        }
-
-        void AddRange(const std::initializer_list<T>& List)
-        {
-            HYDRO_ASSERT(m_Count + List.size() < N, "Cannot add that much element to array. Not enough size");
-            std::copy(List.begin(), List.end(), &m_Data[m_Count]);
-            m_Count += List.size();
-        }
-
-        void AddRange(ConstPointerType Data, SizeType Count)
-        {
-            HYDRO_ASSERT(m_Count + Count < N, "Cannot add that much element to array. Not enough size");
-            std::copy(Data, Data + Count, &m_Data[m_Count]);
-            m_Count += Count;
-        }
-
+        
         bool Contains(ConstReferenceType Element)
         {
-            for(SizeType i = 0; i < m_Count; ++i)
+            for(SizeType i = 0; i < N; ++i)
                 if(m_Data[i] == Element)
                     return true;
             return false;
@@ -116,25 +95,26 @@ namespace Hydro
 
         SizeType Find(ConstReferenceType Element)
         {
-            for(SizeType i = 0; i < m_Count; ++i)
+            for(SizeType i = 0; i < N; ++i)
                 if(m_Data[i] == Element)
                     return i;
             return -1;
         }
         
         Iterator<T> begin() override { return m_Data; }
-        Iterator<T> end() override { return m_Data + m_Count; }
+        Iterator<T> end() override { return m_Data + N; }
         
         ConstIterator<T> begin() const override{ return m_Data; }
-        ConstIterator<T> end() const override { return (PointerType)(&m_Data[0] + m_Count); }
+        ConstIterator<T> end() const override { return (PointerType)(&m_Data[0] + N); }
 
         ConstPointerType Data() const { return m_Data; }
         PointerType Data() { return m_Data; }
 
-        SizeType Count() const { return m_Count; }
+        constexpr SizeType Count() const { return N; }
+
+        operator PointerType() { return m_Data; }
+        operator ConstPointerType() const { return m_Data; }
     private:
         ValueType m_Data[N];
-        SizeType m_Count = 0;
-        
     };
 }
