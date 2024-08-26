496e1e961a48f96d4634c65b56113cab89579c85
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-26 05:38:59 +0200

    Buffer bug fix

--- Source/Core/Buffer.h
+++ Source/Core/Buffer.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "Core/Iterator.h"
 #include <initializer_list>
 #include <algorithm>
 
@@ -8,15 +9,18 @@ namespace Hydro
     class Buffer
     {
     public:
-        Buffer(Type* buffer, size_t count) : m_Count(count)
+        using Iterator = Iterator<Type>;
+        using ConstIterator = ConstIterator<Type>;
+        
+        Buffer(Type* buffer, size_t Count)
         {
-            Allocate(m_Count);
+            Allocate(Count);
             std::copy(buffer, buffer + m_Count, m_Data);
         }
 
-        Buffer(const std::initializer_list<Type>& List) : m_Count(List.size())
+        Buffer(const std::initializer_list<Type>& List)
         {
-            Allocate(m_Count);
+            Allocate(List.size());
             std::copy(List.begin(), List.end(), m_Data);
         }
 
@@ -26,13 +30,39 @@ namespace Hydro
             std::copy(Buffer.GetData(), Buffer.GetData() + Buffer.Count(), m_Data);
         }
 
-        Buffer(Buffer&& Buff) noexcept
+        Buffer(Buffer&& Other) noexcept
         {
-            Allocate(Buff.Count());
-            std::move(Buff.GetData(), Buff.GetData() + Buff.Count(), m_Data);
+            m_Data = Other.m_Data;
+            m_Count = Other.m_Count;
+            Other.m_Data = nullptr;
+            Other.m_Count = 0;
         }
 
-        Buffer(size_t Count) : m_Count(Count)
+        Buffer& operator=(const Buffer& Other)
+        {
+            if(this == &Other)
+                return *this;
+
+            Allocate(Other.Count());
+            std::copy(Other.GetData(), Other.GetData() + Other.Count(), m_Data);
+            return *this;
+        }
+
+        Buffer& operator=(Buffer&& Other)
+        {
+            if(this == &Other)
+                return *this;
+
+            m_Data = Other.m_Data;
+            m_Count = Other.m_Count;
+            Other.m_Data = nullptr;
+            Other.m_Count = 0;
+            return *this;
+        }
+
+        
+
+        Buffer(size_t Count)
         {
             Allocate(Count);
         }
@@ -83,9 +113,11 @@ namespace Hydro
         {
             return m_Data[Index];
         }
-        
-        const Type* Begin() const { return m_Data; }
-        const Type* End() const { return m_Data + m_Count; }
+
+        Iterator begin() { return m_Data; }
+        Iterator end() { return m_Data + m_Count; }
+        ConstIterator begin() const { return m_Data; }
+        ConstIterator end() const { return m_Data + m_Count; }
     private:
         Type* m_Data{nullptr};
         size_t m_Count{0};
@@ -144,8 +176,8 @@ namespace Hydro
     class BufferView 
     {
     public:
-        using Iterator = Type*;
-        using ConstIterator = const Type*;
+        using Iterator = typename Buffer<Type>::Iterator;
+        using ConstIterator = typename Buffer<Type>::Iterator;
         
         BufferView(const Type* Data, size_t Count) : m_Data(const_cast<Type*>(Data)), m_Count(Count){}
         BufferView(const Buffer<Type>& Buff) : m_Data(const_cast<Type*>(Buff.GetData())), m_Count(Buff.Count()){}
@@ -162,7 +194,7 @@ namespace Hydro
         ConstIterator End() const { return m_Data + m_Count; }
 
         template<typename OtherType>
-        BufferView<OtherType> As()
+        BufferView<OtherType> As() const
         {
             return BufferView<OtherType>((OtherType*)GetData(), Count() * (sizeof(Type) / sizeof(OtherType)));
         }
@@ -182,6 +214,12 @@ namespace Hydro
             return m_Data[Index];
         }
 
+        Type& operator[](size_t Index)
+        {
+            return m_Data[Index];
+        }
+        
+
         const Type* GetData() const { return m_Data; }
 
         void CopyTo(Buffer<Type>& Out) const
@@ -189,9 +227,6 @@ namespace Hydro
             Out.Allocate(m_Count);
             std::copy(m_Data, m_Data + m_Count, Out.GetData());
         }
-
-        Iterator begin() const { return m_Data; }
-        Iterator end() const { return m_Data; }
     private:
         Type* m_Data;
         size_t m_Count;
