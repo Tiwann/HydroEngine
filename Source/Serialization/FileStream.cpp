c35fb73d782cc4af30c4b7da63a28a21fc80bbbb
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-26 05:48:27 +0200

    FileStream implementation

---
+++ Source/Serialization/FileStream.cpp
@@ -0,0 +1,106 @@
+﻿#include "FileStream.h"
+#include <cstdio>
+
+namespace Hydro
+{
+    static StringView GetMode(OpenModeFlags OpenMode)
+    {
+        switch (OpenMode) {
+        case OpenModeFlagBits::Read: return "r";
+        case OpenModeFlagBits::Write: return "w";
+        case OpenModeFlagBits::ReadText: return "r";
+        case OpenModeFlagBits::ReadBinary: return "rb";
+        case OpenModeFlagBits::WriteText: return "w";
+        case OpenModeFlagBits::WriteBinary: return "wb";
+        case OpenModeFlagBits::ReadWriteText: return "w+";
+        case OpenModeFlagBits::ReadWriteBinary: return "wb+";
+        default: return "";
+        }
+    }
+    
+    FileStream::FileStream(Path Filepath, OpenModeFlags OpenMode): Stream(OpenMode), m_Filepath(std::move(Filepath))
+    {
+        m_Handle = fopen(m_Filepath.string().c_str(), GetMode(OpenMode));
+        m_Opened = m_Handle;
+    }
+    
+    bool FileStream::Open(const Path& Filepath, OpenModeFlags OpenMode)
+    {
+        if(m_Opened) Close();
+        m_Filepath = Filepath;
+        m_OpenMode = OpenMode;
+
+        m_Handle = fopen(m_Filepath.string().c_str(), GetMode(OpenMode));
+        return m_Opened = m_Handle;
+    }
+
+    Stream::SizeType FileStream::Read(void* OutBuffer, SizeType Count)
+    {
+        return fread(OutBuffer, 1, Count, m_Handle);
+    }
+
+    Stream::SizeType FileStream::Write(const void* InBuffer, SizeType Count)
+    {
+        return fwrite(InBuffer, 1, Count, m_Handle);
+    }
+
+    bool FileStream::Seek(Hydro::Seek SeekMode, OffsetType Offset)
+    {
+        const auto GetSeekMode = [](Hydro::Seek SeekMode) -> int
+        {
+            return SeekMode == Seek::Begin ? SEEK_SET : SeekMode == Seek::Current ? SEEK_CUR : SEEK_END;
+        };
+            
+        const int Result = fseek(m_Handle, Offset, GetSeekMode(SeekMode));
+        return Result == 0;
+    }
+
+    Stream::OffsetType FileStream::Tell() const
+    {
+        return ftell(m_Handle);
+    }
+
+    void FileStream::Close()
+    {
+        Stream::Close();
+        (void)fclose(m_Handle);
+        m_Handle = nullptr;
+    }
+
+
+    StandardStream StandardStream::StandardIn = StandardStream(stdin, OpenModeFlagBits::ReadText);
+    StandardStream StandardStream::StandardOut = StandardStream(stdout, OpenModeFlagBits::WriteText);
+    StandardStream StandardStream::StandardError = StandardStream(stderr, OpenModeFlagBits::WriteText);
+    
+    StandardStream::StandardStream(FILE* Handle, const OpenModeFlags& OpenMode): Stream(OpenMode), m_Handle(Handle)
+    {
+        m_Opened = m_Handle;
+    }
+
+    Stream::SizeType StandardStream::Read(void* OutBuffer, SizeType Count)
+    {
+        return -1;
+    }
+
+    Stream::SizeType StandardStream::Write(const void* InBuffer, SizeType Count)
+    {
+        return fprintf(m_Handle, "%s", (const char*)InBuffer);
+    }
+
+    bool StandardStream::Seek(Hydro::Seek SeekMode, OffsetType Offset)
+    {
+        return false;
+    }
+
+    Stream::OffsetType StandardStream::Tell() const
+    {
+        return -1;
+    }
+
+    void StandardStream::Close()
+    {
+        Stream::Close();
+    }
+
+    
+}
---
+++ Source/Serialization/FileStream.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "Stream.h"
+
+namespace Hydro
+{
+    class FileStream : public Stream
+    {
+    public:
+        explicit FileStream(Path Filepath, OpenModeFlags OpenMode);
+        
+        bool Open(const Path& Filepath, OpenModeFlags OpenMode);
+        SizeType Read(void* OutBuffer, SizeType Count) override;
+        SizeType Write(const void* InBuffer, SizeType Count) override;
+        bool Seek(Hydro::Seek SeekMode, OffsetType Offset) override;
+        OffsetType Tell() const override;
+        void Close() override;
+        
+    private:
+        Path m_Filepath;
+        FILE* m_Handle = nullptr;
+    };
+
+    class StandardStream : public Stream
+    {
+    public:
+        SizeType Read(void* OutBuffer, SizeType Count) override;
+        SizeType Write(const void* InBuffer, SizeType Count) override;
+        bool Seek(Hydro::Seek SeekMode, OffsetType Offset) override;
+        OffsetType Tell() const override;
+        void Close() override;
+        static StandardStream StandardOut;
+        static StandardStream StandardIn;
+        static StandardStream StandardError;
+        
+    protected:
+        explicit StandardStream(FILE* Handle, const OpenModeFlags& OpenMode);
+    
+    private:
+        FILE* m_Handle;
+    };
+}
