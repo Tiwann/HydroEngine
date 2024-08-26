f356616385ad035626b554f0882d56411be3a9e3
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-26 05:48:47 +0200

    Working on a Xml writer

---
+++ Source/Serialization/XmlWriter.cpp
@@ -0,0 +1,64 @@
+﻿#include "XmlWriter.h"
+
+namespace Hydro
+{
+    void XmlWriter::BeginDocument(int32_t VersionMaj, int32_t VersionMin)
+    {
+        const String Version = Format("{}.{}", VersionMaj, VersionMin);
+        const String BeginDocument = Format("<?xml version=\"{}\"?>\n", Version);
+        m_Stream->WriteString(BeginDocument);
+        m_DocumentBegun = true;
+    }
+
+    template <typename ValueType>
+    void XmlWriter::BeginElement(const String& Name, const Map<String, ValueType>& Attributes)
+    {
+        HYDRO_ASSERT(m_DocumentBegun, "Bad Xml: Did you call BeginDocument() ?");
+        Array<String> FormattedAttributes;
+        for(const Pair<String, ValueType>& Attribute : Attributes)
+            FormattedAttributes.Add(Format("{}=\"{}\"", Attribute.Key, Attribute.Value));
+            
+        String Content;
+        Content.Append(Name);
+        for(const String& Attribute : FormattedAttributes)
+            Content.Append(Format(" {}", Attribute));
+
+        const String Tag = Format("{}<{}>\n", GetIndentation(), Content);
+        m_Stream->WriteString(Tag);
+        m_NumIndentation++;
+        m_Elements.Add(Name);
+    }
+
+    template <typename ValueType>
+    void XmlWriter::ElementInline(const String& Name, const Map<String, ValueType>& Attributes)
+    {
+        HYDRO_ASSERT(m_DocumentBegun, "Bad Xml: Did you call BeginDocument() ?");
+        Array<String> FormattedAttributes;
+        for(const Pair<String, ValueType>& Attribute : Attributes)
+            FormattedAttributes.Add(Format("{}=\"{}\"", Attribute.Key, Attribute.Value));
+            
+        String Content;
+        Content.Append(Name);
+        for(const String& Attribute : FormattedAttributes)
+            Content.Append(Format(" {}", Attribute));
+
+        const String Tag = Format("{}<{}/>\n", GetIndentation(), Content);
+        m_Stream->WriteString(Tag);
+    }
+
+    void XmlWriter::EndElement()
+    {
+        m_NumIndentation--;
+        const String Tag = Format("{}</{}>\n", GetIndentation(), m_Elements.Last());
+        m_Stream->WriteString(Tag);
+        m_Elements.Pop();
+    }
+
+    String XmlWriter::GetIndentation()
+    {
+        String Indentation = "";
+        for(uint32_t i = 0; i < m_NumIndentation * m_Settings.Indentation; i++)
+            Indentation.Append(m_Settings.IndentationCharacter);
+        return Indentation;
+    }
+}
---
+++ Source/Serialization/XmlWriter.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "Stream.h"
+#include "Core/Containers/String.h"
+#include "Core/Containers/Map.h"
+#include "Core/Assertion.h"
+
+namespace Hydro
+{
+    struct XmlSettings
+    {
+        int32_t Indentation = 4;
+        char    IndentationCharacter = ' ';
+    };
+
+    
+    class XmlWriter final
+    {
+    public:
+        XmlWriter(Stream& Stream, const XmlSettings& Settings = {}) : m_Stream(&Stream), m_Settings(Settings) {}
+        ~XmlWriter()
+        {
+            HYDRO_ASSERT(m_NumIndentation == 0, "Bad Xml: Begin/End mismatch!");
+        }
+        
+        void BeginDocument(int32_t VersionMaj, int32_t VersionMin);
+
+        template<typename ValueType>
+        void BeginElement(const String& Name, const Map<String, ValueType>& Attributes);
+
+        template<typename ValueType>
+        void ElementInline(const String& Name, const Map<String, ValueType>& Attributes);
+        
+        void EndElement();
+
+        
+
+    private:
+        Stream* m_Stream;
+        XmlSettings m_Settings;
+        Array<String> m_Elements;
+        uint32_t m_NumIndentation = 0;
+        bool m_DocumentBegun = false;
+
+        String GetIndentation();
+    };
+}
