55cd6beddcecbb829e1be385edbee760953cb4e3
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-26 05:47:24 +0200

    Working on MemoryStream

---
+++ Source/Serialization/MemoryStream.cpp
@@ -0,0 +1,51 @@
+﻿#include "MemoryStream.h"
+
+namespace Hydro
+{
+    Stream::SizeType MemoryStream::Read(void* OutBuffer, SizeType Count)
+    {
+        if(!m_Opened) return -1;
+        memcpy(OutBuffer, &m_Buffer[m_Position], Count);
+        m_Position += (OffsetType)Count;
+        return Count;
+    }
+
+    Stream::SizeType MemoryStream::Write(const void* InBuffer, SizeType Count)
+    {
+        if(!m_Opened) return -1;
+        if(m_Position + Count > m_Buffer.Count()) return -1;
+        memcpy(&m_Buffer[m_Position], InBuffer, Count);
+        m_Position += (OffsetType)Count;
+        return Count;
+    }
+
+    bool MemoryStream::Seek(Hydro::Seek SeekMode, OffsetType Offset)
+    {
+        if(!m_Opened) return false;
+        switch (SeekMode) {
+        case Seek::Begin:
+            if(Offset < 0) return false;
+            m_Position = Offset;
+            return true;
+        case Seek::Current:
+            if(m_Position + Offset < 0) return false;
+            m_Position += Offset;
+            return true;
+        case Seek::End:
+            if(Offset > 0) return false;
+            m_Position = (OffsetType)m_Buffer.Count() + Offset;
+            return true;
+        }
+        return false;
+    }
+
+    Stream::OffsetType MemoryStream::Tell() const
+    {
+        return m_Opened ? m_Position : -1;
+    }
+
+    void MemoryStream::Close()
+    {
+        Stream::Close();
+    }
+}
---
+++ Source/Serialization/MemoryStream.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Stream.h"
+
+namespace Hydro
+{
+    class MemoryStream : public Stream
+    {
+    public:
+        explicit MemoryStream(const BufferView<uint8_t>& InBuffer, OpenModeFlags OpenMode)
+        : Stream(OpenMode), m_Buffer(InBuffer) { m_Opened = true; }
+
+        explicit MemoryStream(const BufferView<char>& InBuffer, OpenModeFlags OpenMode)
+        : Stream(OpenMode), m_Buffer(InBuffer.As<uint8_t>()) { m_Opened = true; }
+        
+        SizeType    Read(void* OutBuffer, SizeType Count) override;
+        SizeType    Write(const void* InBuffer, SizeType Count) override;
+        bool        Seek(Hydro::Seek SeekMode, OffsetType Offset) override;
+        OffsetType  Tell() const override;
+        void        Close() override;
+    private:
+        BufferView<uint8_t> m_Buffer;
+        OffsetType m_Position = 0;
+    };
+}
