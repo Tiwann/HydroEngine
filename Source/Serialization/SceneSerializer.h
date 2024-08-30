b5ed0e2c6d0c8a85d4f228d641287e798fa062fc
Erwann Messoah <tiwannbeats@gmail.com>
2024-08-30 14:51:51 +0200

    Update SceneSerializer.h

--- Source/Serialization/SceneSerializer.h
+++ Source/Serialization/SceneSerializer.h
@@ -12,11 +12,8 @@ namespace Hydro
     {
     public:
         bool Serialize(const Ref<Scene>& Scene, const Path& Filepath) override;
-
         bool Deserialize(Ref<Scene>& Scene, const Path& Filepath) override;
-
         bool SerializeMemory(const Ref<Scene>& Scene, std::stringstream& Stream) override;
-
         bool DeserializeMemory(const std::stringstream& Stream, Ref<Scene>& Scene) override;
 
     protected:
