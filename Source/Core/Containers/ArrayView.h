e91cfb499229a71bf18555cae8cfb851d3065351
Tiwann <tiwannbeats@gmail.com>
2024-09-01 22:33:26 +0200

    Im an idiot

--- Source/Core/Containers/ArrayView.h
+++ Source/Core/Containers/ArrayView.h
@@ -4,25 +4,35 @@
 namespace Hydro
 {
     template<typename T>
-    class StringViewBase
+    class ArrayView
     {
         using ArrayType = Array<T>;
         using PointerType = typename ArrayType::PointerType;
         using ConstPointerType = typename ArrayType::ConstPointerType;
+        using ConstReferenceType = typename ArrayType::ConstReferenceType;
         using SizeType = typename ArrayType::SizeType;
         using ConstIterator = typename ArrayType::ConstIterator;
     public:
-        StringViewBase(const ArrayType& Arr) : m_Data(Arr.Data()), m_Count(Arr.Count()){}
-        StringViewBase(const StringViewBase&) = default;
-        StringViewBase(StringViewBase&&) noexcept = default;
-        StringViewBase& operator=(const StringViewBase&) = default;
-        StringViewBase& operator=(StringViewBase&&) noexcept = default;
+        ArrayView() : m_Data(nullptr), m_Count(0) {}
+        ArrayView(const ArrayType& Arr) : m_Data(Arr.Data()), m_Count(Arr.Count()){}
+        ArrayView(const ArrayView&) = default;
+        ArrayView(ArrayView&&) noexcept = default;
+        ArrayView& operator=(const ArrayView&) = default;
+        ArrayView& operator=(ArrayView&&) noexcept = default;
 
         ConstPointerType Data() const { return m_Data; }
         SizeType Count() const { return m_Count; }
+        bool IsEmpty() const { return m_Count == 0; }
 
         ConstIterator begin() const { return m_Data; }
         ConstIterator end() const { return m_Data + m_Count; }
+
+        ConstReferenceType operator[](SizeType Index) const { return m_Data[Index]; }
+
+        bool operator==(const ArrayView& other) const
+        {
+            return m_Data == other.m_Data && m_Count == other.m_Count;
+        }
     private:
         ConstPointerType m_Data = nullptr;
         SizeType m_Count = 0;
